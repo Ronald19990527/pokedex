055dd97cb0ef0b9ac409aeedb59c8a2e2fc68c2a
Ronald19990527 <ronaldhernandezdp@unimagdalena.edu.co>
2024-08-13 22:20:07 -0500

    Adding pokemons search by id or name

--- search-pokemons-by-id-or-name.js
+++ search-pokemons-by-id-or-name.js
@@ -6,13 +6,72 @@ export default function searchPokemonsByIdOrName(writeNameId, search, pokemons) 
     $pokemons = d.querySelector(pokemons);
 
     $search.addEventListener("click", () => {
+        $pokemons.classList.remove("flex-row-start");
+
         if ($writeNameId.value !== "") getDataPokemon($writeNameId.value);
-        else $pokemons.innerHTML = `
-                <p>First enter pokemon's name or id</p>
-                <img src="sticker-png-pikachu-i-choose-you-sad-icon-thumbnail.png" alt="Not found pokemon">
+        else {
+            $pokemons.innerHTML = `
+                <article style="background-color: var(--black-color); color: var(--white-color); text-align: center;">
+                    <h2><i>First enter pokemon's name or id</i></h2>
+                    <img alt="Not found pokemon" class="w-h-max" src="adesivi-poke-ball---pokemon.jpg">
+                </article>
             `;
-        console.log($writeNameId.value);
+        }
     });
 
-    async function getDataPokemon(pokemon) {}
+    async function getDataPokemon(pokemon) {
+        try {
+            let res = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.toLowerCase()}/`),
+            json = await res.json(),
+            types = [];
+
+            if (!res.ok) throw { status: res.status, statusText: res.statusText };
+
+            json.types.forEach((el) => {
+                console.log(el);
+
+                types.push(el.type.name);
+            });
+
+            $pokemons.innerHTML = `
+                <article class="align-items-center flex-row-start">
+                    <div>
+                        <img src=${json.sprites.other.home.front_default ? json.sprites.other.home.front_default : "adesivi-poke-ball---pokemon.jpg"}>
+                    </div>
+                    <div>
+                        <h2>${json.name}</h2>
+                        <h3>N.°${json.id}</h3>
+                        <div>
+                            <h5>Data</h5>
+                            <div class="flex-row-start">
+                                <div>
+                                    <h4>Height</h4>
+                                    <small>${json.height} m</small>
+                                    <h4>Types</h4>
+                                    ${types}
+                                </div>
+                                <div>
+                                    <h4>Weight</h4>
+                                    <small>${json.weight} kg</small>
+                                    <h4>Sound</h4>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </article>
+            `;
+
+            console.log(json);
+        } catch (err) {
+            let message = err.statusText || "Ocurrió un error";
+            console.log(`Error ${err.status}: ${message}`);
+
+            $pokemons.innerHTML = `
+                <article style="background-color: var(--black-color); color: var(--white-color); text-align: center;">
+                    <h2><i>Not found pokemon with these specifications</i></h2>
+                    <img alt="Not found pokemon" class="w-h-max" src="adesivi-poke-ball---pokemon.jpg">
+                </article>
+            `;
+        }
+    }
 }
--- search-pokemons-by-id.js
+++ search-pokemons-by-id.js
@@ -16,6 +16,8 @@ export default function searchPokemonsById(pokemons) {
 
             if (!res.ok) throw { status: res.status, statusText: res.statusText };
 
+            if (!$pokemons.classList.contains("flex-row-start")) $pokemons.classList.add("flex-row-start");
+
             $fragment.innerHTML = "";
             $pokemons.innerHTML = "";
 
@@ -53,39 +55,38 @@ export default function searchPokemonsById(pokemons) {
     
             if (!res.ok) throw { status: res.status, statusText: res.statusText };
 
-            if (json.sprites.other.home.front_default) {
-                const $card = d.createElement("article"),
-                $image = d.createElement("img"),
-                $pokemonName = d.createElement("h3"),
-                $abilities = d.createElement("h5"),
-                $abilitiesList = d.createElement("ul");
+            const $card = d.createElement("article"),
+            $image = d.createElement("img"),
+            $pokemonName = d.createElement("h3"),
+            $abilities = d.createElement("h5"),
+            $abilitiesList = d.createElement("ul");
 
-                $image.setAttribute("src", json.sprites.other.home.front_default);
-                $pokemonName.classList.add("mg-bottom-1rem");
-                $pokemonName.classList.add("pd-x-0-5rem");
-                $abilities.classList.add("mg-bottom-1rem");
-                $abilities.classList.add("pd-x-0-5rem");
-                if (json.name) $image.setAttribute("alt", json.name);
-                $pokemonName.textContent = json.name;
-                $abilities.textContent = "Abilities";
-                json.abilities.forEach((el) => {
-                    const $li = d.createElement("li");
+            if (json.sprites.other.home.front_default) $image.setAttribute("src", json.sprites.other.home.front_default);
+            else $image.setAttribute("src", "adesivi-poke-ball---pokemon.jpg");
+            $pokemonName.classList.add("mg-bottom-1rem");
+            $pokemonName.classList.add("pd-x-0-5rem");
+            $abilities.classList.add("mg-bottom-1rem");
+            $abilities.classList.add("pd-x-0-5rem");
+            if (json.name) $image.setAttribute("alt", json.name);
+            $pokemonName.textContent = json.name;
+            $abilities.textContent = "Abilities";
+            json.abilities.forEach((el) => {
+                const $li = d.createElement("li");
 
-                    $li.classList.add("pd-y-0-5rem");
-                    $li.innerHTML = `<small class="pd-x-0-5rem">${el.ability.name}</small>`;
+                $li.classList.add("pd-y-0-5rem");
+                $li.innerHTML = `<small class="pd-x-0-5rem">${el.ability.name}</small>`;
 
-                    $abilitiesList.appendChild($li);
-                });
+                $abilitiesList.appendChild($li);
+            });
 
-                $card.appendChild($image);
-                $card.appendChild($pokemonName);
-                $card.appendChild($abilities);
-                $card.appendChild($abilitiesList);
-                $card.classList.add("card");
-                $card.classList.add("pd-x-0-5rem");
+            $card.appendChild($image);
+            $card.appendChild($pokemonName);
+            $card.appendChild($abilities);
+            $card.appendChild($abilitiesList);
+            $card.classList.add("card");
+            $card.classList.add("pd-x-0-5rem");
 
-                $fragment.appendChild($card);
-            }
+            $fragment.appendChild($card);
         } catch (err) {
             let message = err.statusText || "Ocurrió un error";
             $pokemons.innerHTML = `Error ${err.status}: ${message}`;
