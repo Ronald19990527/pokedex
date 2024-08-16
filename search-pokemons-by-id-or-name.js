3ce1f5ec97ff3fedf4603ee52e52d9e3a8348222
Ronald19990527 <ronaldhernandezdp@unimagdalena.edu.co>
2024-08-15 22:14:10 -0500

    adding footer

--- search-pokemons-by-id-or-name.js
+++ search-pokemons-by-id-or-name.js
@@ -26,7 +26,7 @@ export default function searchPokemonsByIdOrName(writeNameId, search, pokemons) 
             types = ``,
             typesCount = 0;
 
-            if (!res.ok) throw { status: res.status, statusText: res.statusText };
+            if (!res.ok) throw {};
 
             json.types.forEach((el) => {
                 typesCount++;
@@ -55,12 +55,23 @@ export default function searchPokemonsByIdOrName(writeNameId, search, pokemons) 
                                     <h4 class="mg-y-0-5rem">Weight</h4>
                                     <small>${json.weight} kg</small>
                                     <h4 class="mg-y-0-5rem">Sound</h4>
+                                    <button class="bg-color-gray border-radios-0-5rem button-play pd-0-5rem">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="var(--white-color)" class="bi bi-play-circle-fill" viewBox="0 0 16 16">
+                                            <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M6.79 5.093A.5.5 0 0 0 6 5.5v5a.5.5 0 0 0 .79.407l3.5-2.5a.5.5 0 0 0 0-.814z"/>
+                                        </svg>
+                                        <small class="color-white">Play</small>
+                                    </button>
+                                    <audio src=${json.cries.latest ? json.cries.latest : json.cries.latest.legacy}></audio>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </article>
             `;
+
+            d.querySelector(".button-play").addEventListener("click", () => {
+                d.querySelector("audio").play();
+            });
         } catch (err) {
             $pokemons.innerHTML = `
                 <article style="background-color: var(--black-color); color: var(--white-color); text-align: center;">
